7809ad9446f12b45e768ad66b04e9f9e94f9cab4
Leandro Rojano <leandrorojano2001@hotmail.com>
2024-08-14 23:13:40 -0300

    Agregado animaciones links header

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from 'react';
-import { Button, Box, Flex, IconButton } from '@chakra-ui/react'
+import { Button, Box, Flex, IconButton, HStack } from '@chakra-ui/react'
 import { Menu, MenuButton, MenuList, MenuItem, Text, useColorModeValue } from '@chakra-ui/react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faList } from '@fortawesome/free-solid-svg-icons';
 import { colors } from '../../themes/themes.js'
 import {Link, Events} from 'react-scroll'
 import { DarkModeSwitch } from '../DarkModeSwitch/DarkModeSwitch';
+import { motion } from "framer-motion";
 
 
 const Header=()=>{
@@ -14,7 +15,7 @@ const Header=()=>{
     const color = useColorModeValue(colors.n, colors.b)
 
     const marginLinks= '10px';
-    const fS1 = [null,null,null, '14px','18px'] //fontSize
+    const fS1 = [null,null,'10px', '12px','18px'] //fontSize
     const fW1 = 600 //fontWeight
 
     // Propiedades React-scroll
@@ -45,6 +46,95 @@ const Header=()=>{
         };
       }, []);
 
+    // Funciones Animación de Links
+    const DURATION = 0.25;
+    const STAGGER = 0.025;
+
+const FlipLink = ({ children, href }) => {
+  return (
+    <motion.div
+      initial="initial"
+      whileHover="hovered"
+      style={{
+        position: "relative",
+        display: "block",
+        overflow: "hidden",
+        whiteSpace: "nowrap",
+        textTransform: "uppercase",
+        lineHeight: 1,
+        letterSpacing: "0em",
+      }}
+    >
+
+      {/* Link de arriba */}
+      <Link to={href} smooth={"easeInOutQuint"} duration={1000} offset={-140} _hover={{ textDecoration: "none"}} inset={0}>
+        <Flex>
+          {children.split("").map((l, i) => (
+            <motion.span
+              variants={{
+                initial: {
+                  y: 0,
+                },
+                hovered: {
+                  y: "-100%",
+                },
+              }}
+              transition={{
+                duration: DURATION,
+                ease: "easeInOut",
+                delay: STAGGER * i,
+              }}
+              style={{ display: "inline-block",
+                textDecoration:"none",
+                fontSize:{fS1},
+                fontWeight:"900",
+                letterSpacing: "0em",
+                textAlign:"right",
+                justifyContent:"right"
+              }}
+              key={i}
+            >
+              {l}
+            </motion.span>
+          ))}
+        </Flex>
+
+        {/* Link de abajo */}
+        <Flex position="absolute" inset={0}>
+          {children.split("").map((l, i) => (
+            <motion.span
+              variants={{
+                initial: {
+                  y: "100%",
+                },
+                hovered: {
+                  y: 0,
+                },
+              }}
+              transition={{
+                duration: DURATION,
+                ease: "easeInOut",
+                delay: STAGGER * i,
+              }}
+              style={{ display: "inline-block",
+                textDecoration:"none",
+                fontSize:{fS1},
+                fontWeight:"900",
+                letterSpacing: "0em",
+                textAlign:"right",
+                justifyContent:"right"
+               }}
+              key={i}
+            >
+              {l}
+            </motion.span>
+          ))}
+        </Flex>
+      </Link>
+    </motion.div>
+  );
+};
+
     return(
         <Flex 
         as="header"
@@ -54,7 +144,7 @@ const Header=()=>{
         position="sticky"
         top={['10px',null,null,'50px']}
         margin="0 auto"
-        padding="0 60px"
+        padding="0 60px 0 50px"
         zIndex="100"
         bg={bg}
         boxShadow="0 5px 32px rgba(0, 0, 0, 0.05)"
@@ -68,28 +158,29 @@ const Header=()=>{
 
             <Flex as='section' display={['none',null,null,'flex']}flexDirection='row' justifyContent='space-between' width='100%' >
                 <Flex as='div' className="header-izq" alignItems="center" color={colors.color2} flexShrink='0'>
-                    <Link to="hero" smooth={smooth} duration={duration} offset={offset} onClick={() => handleLinkClick('hero')}>
+                    <Link to="hero" smooth={smooth} duration={duration} offset={offset}>
                         <Text as={("h2")} fontWeight={"800"} color={color} fontSize={"1.5em"} fontFamily={"montserrat"}>LR<Text as={"span"} color={colors.v} fontSize={"1em"}>.</Text>Dev</Text>
                     </Link>
                 </Flex>
                 
-                <Flex as='nav' alignItems='center' justifyContent='right' gap='10px' pr='10px'color={colors.color2} flex='1' overflow='hidden' style={{cursor: "pointer"}} >
-                <Link to="services" smooth={smooth} duration={duration} offset={offset} onClick={() => handleLinkClick('services')} > <Text as={"a"} color={color}>Servicios</Text></Link>
-                    
-                    <Link to="skills" smooth={smooth} duration={duration} offset={offset}  fontSize={fS1} fontWeight={fW1} mx={marginLinks} cursor={"pointer"}><Text as={"a"} color={color}>Habilidades</Text></Link>
+                <Flex as='nav' alignItems='center' justifyContent='right' gap='10px' pr='10px'color={colors.color2} flex='1' style={{cursor: "pointer"}} >
+                
+                    <FlipLink href="services" color="red">Servicios</FlipLink>
 
-                    <Link to="portfolio" smooth={smooth} duration={duration} offset={offset}  fontSize={fS1} fontWeight={fW1} mx={marginLinks} cursor={"pointer"}><Text as={"a"} color={color}>Portfolio</Text></Link>
+                    <FlipLink href="skills">Habilidades</FlipLink>
 
-                    <Link to="testimonials" smooth={smooth} duration={duration} offset={offset}  fontSize={fS1} fontWeight={fW1} mx={marginLinks} cursor={"pointer"}><Text as={"a"} color={color}>Testimonios</Text></Link>
+                    <FlipLink href="portfolio">Portfolio</FlipLink>
 
-                    <Link to="prices" smooth={smooth} duration={duration} offset={offset}  fontSize={fS1} fontWeight={fW1} mx={marginLinks} cursor={"pointer"}><Text as={"a"} color={color}>Precios</Text></Link>
+                    <FlipLink href="testimonials">Testimonios</FlipLink>
+
+                    <FlipLink href="prices">Precios</FlipLink>
                     
                     <Box as={FontAwesomeIcon} icon={faList} display='none'></Box>  
                 </Flex>
 
                 <Box className="div-boton" display="flex" alignItems='center' marginLeft='auto' >
                     <Link to="contact" smooth={smooth} duration={duration} offset={offset} >
-                        <Button bg={colors.v} color={color} borderRadius='20px' fontSize={fS1} fontWeight={fW1} width={[null,null,null,'110px','140px','160px']}>Contacto</Button>
+                        <Button bg={colors.v} color={color} borderRadius='20px' fontSize={fS1} fontWeight={fW1} width={[null,null,null,'60px','140px','160px']}>Contacto</Button>
                     </Link>
                     <DarkModeSwitch />
                 </Box>
@@ -103,6 +194,7 @@ const Header=()=>{
                 </Link>
             </Flex>
 
+        
                 {/* Menú Hamburguesa para tamaños de pantalla pequeñas */}
                 <Menu position="absolute" top={4} right={4} isOpen={isOpen}>
                     <MenuButton as={IconButton} onClick={handleMenuClick} aria-label='Options'
