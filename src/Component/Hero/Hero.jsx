dc6998ccacc983b47e041b1351df174b3c76481f
Leandro Rojano <leandrorojano2001@hotmail.com>
2024-08-16 01:40:05 -0300

    Agregado animaciones

--- src/Component/Hero/Hero.jsx
+++ src/Component/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import { Box, Flex , Text, Img, Button, useColorModeValue} from "@chakra-ui/reac
 import { motion } from "framer-motion"
 import { colors } from "../../themes/themes"
 import { Reveal } from "../Reveal/Reveal"
+import WaterDropGrid from '/src/Component/WaterDropGrid/WaterDropGrid.jsx';
 
 const Hero = () => {
 
@@ -10,8 +11,8 @@ const Hero = () => {
 
     return(
         <>
-        <Flex as='section' alignItems={"center"} justifyContent={"center"} p={0} flexDirection={['column',null,null,'row']} id="hero" mt={"30px"}>
-            <Flex className="hero-1" w={['90%',null,null,'50%']} p={['0',null,null,'0 0 0 50px']} textAlign={"left"} flexDirection={"column"}>
+        <Flex as='section' alignItems={"center"} zIndex={3} justifyContent={"center"} p={0} flexDirection={['column',null,null,'row']} id="hero" mt={"30px"}>
+            <Flex className="hero-1"  zIndex={3} w={['90%',null,null,'50%']} p={['0',null,null,'0 0 0 50px']} textAlign={"left"} flexDirection={"column"}>
 
                 <Reveal>
                 <Flex className="tittle1">
@@ -59,13 +60,13 @@ const Hero = () => {
                     </Flex>
             </Flex>
 
-            <Flex className="hero-2" w={"50%"} flexDirection={"column"} justifyContent={"center"} alignItems={"center"} mt={['50px',null,null,'0px']}>
-                <Flex className="bg" w={['200px',null,null,'400px']} h={['200px',null,null,'400px']} borderRadius={"50%"} overflow={"hidden"} border={"none"} bgGradient='linear(to-tl, #155445, #28a587)'>
+            <Flex className="hero-2" position: relative zIndex={2} w={"50%"} flexDirection={"column"} justifyContent={"center"} alignItems={"center"} mt={['50px',null,null,'0px']}>
+                <Flex className="bg" zIndex={2} w={['200px',null,null,'400px']} h={['200px',null,null,'400px']} borderRadius={"50%"} overflow={"hidden"} border={"none"} bgGradient='linear(to-tl, #155445, #28a587)'>
                 <Box
       as={motion.div}
       animate={{ opacity: 1, translateY: '0px', scale: 1 }}
       initial={{ opacity: 0, translateY: '100px', scale: 0.8 }}
-      transition={{ duration: 30, ease: 'easeInOut' }} // Duración en segundos
+      transition={{ duration: 3, ease: 'easeInOut' }}
       backgroundImage="url('/img/yo.png')"
       backgroundPosition="center"
       backgroundSize={['cover', null, null, 'cover']}
@@ -73,12 +74,16 @@ const Hero = () => {
       width="100%"
       height="100%"
       borderRadius="50%"
-      display="block"
+    //   display="block"
       margin="0 auto"
+      zIndex={2}
     />
                 </Flex>
 
-                <Flex className="pop pop1" mt={['0px',null,null,'-100px']} h={"100%"} position={"relative"} right={['60px',null,null,'130px']} p={"5px 20px"} fontSize={"1rem"} color={color} backgroundColor={bg} border={"none"} borderRadius={"35px"} boxShadow={"0 0 0 3px black, 6px 6px 0px #b1b1b1a1"} alignItems={"center"} justifyContent={"space-between"}>
+                <Flex>
+                </Flex>
+
+                <Flex className="pop pop1" zIndex={2} mt={['0px',null,null,'-100px']} h={"100%"} position={"relative"} right={['60px',null,null,'130px']} p={"5px 20px"} fontSize={"1rem"} color={color} backgroundColor={bg} border={"none"} borderRadius={"35px"} boxShadow={"0 0 0 3px black, 6px 6px 0px #b1b1b1a1"} alignItems={"center"} justifyContent={"space-between"}>
                     <Flex className="pop-cont-num" m={"0"} p={"0"} alignItems={"center"} justifyContent={"space-between"} flexWrap={"0"} fontSize={"2em"}>
                         <Text as={"h4"} m={"0"} p={"0"} fontSize={"1.5rem"}>3<Text as={"span"}>+</Text></Text>
                     </Flex>
@@ -87,7 +92,7 @@ const Hero = () => {
                     </Flex>
                 </Flex>
 
-                <Flex className="pop pop2" mt={"-20px"} h={"100%"} position={"relative"} right={['-70px',null,null,'-130px']} p={"5px 20px"} fontSize={"1rem"} color={color} backgroundColor={bg} border={"none"} borderRadius={"35px"} boxShadow={"0 0 0 3px black, 6px 6px 0px #b1b1b1a1"} alignItems={"center"} justifyContent={"space-between"}>
+                <Flex className="pop pop2" zIndex={2} mt={"-20px"} h={"100%"} position={"relative"} right={['-70px',null,null,'-130px']} p={"5px 20px"} fontSize={"1rem"} color={color} backgroundColor={bg} border={"none"} borderRadius={"35px"} boxShadow={"0 0 0 3px black, 6px 6px 0px #b1b1b1a1"} alignItems={"center"} justifyContent={"space-between"}>
                     <Flex className="pop-cont-num" m={"0"} p={"0"} alignItems={"center"} justifyContent={"space-between"} flexWrap={"0"} fontSize={"2em"}>
                         <Text as={"h4"} m={"0"} p={"0"} fontSize={"1.5rem"}>8</Text>
                     </Flex>
@@ -95,6 +100,7 @@ const Hero = () => {
                         <Flex as={"h5"} m={"5px"} p={"0"} flexDirection={"column"} justifyContent={"left"} fontSize={"0.6em"} fontWeight={"500"} flexWrap={"1"}><Text as={"span"} m={"0 5px 0 0"} p={"0"} color={"var(--hl-color)"}>Proyectos</Text>  Completados</Flex>
                     </Flex>
                 </Flex>
+                    <WaterDropGrid></WaterDropGrid>
             </Flex>
         </Flex>        
         </>
---
+++ src/Component/WaterDropGrid/WaterDropGrid.jsx
@@ -0,0 +1,96 @@
+import { Box, Grid, GridItem } from "@chakra-ui/react";
+import anime from "animejs";
+
+const WaterDropGrid = () => {
+  return (
+    <Box
+    position="absolute"
+    display={["none","grid","grid","grid"]}
+    top={[0,105,100,-150,-220]}
+    left={[0,0,0,220,250]}
+    width="100%"
+    height="100%"
+      // display="grid"
+      placeContent="center"
+      bg="transparent"
+      px={0}
+      py={0}
+      zIndex={1}
+    >
+      <DotGrid />
+    </Box>
+  );
+};
+
+const GRID_WIDTH = 20;
+const GRID_HEIGHT = 15;
+
+const DotGrid = () => {
+  const handleDotClick = (e) => {
+    anime({
+      targets: ".dot-point",
+      scale: [
+        { value: 1.35, easing: "easeOutSine", duration: 250 },
+        { value: 1, easing: "easeInOutQuad", duration: 500 },
+      ],
+      translateY: [
+        { value: -15, easing: "easeOutSine", duration: 250 },
+        { value: 0, easing: "easeInOutQuad", duration: 500 },
+      ],
+      opacity: [
+        { value: 1, easing: "easeOutSine", duration: 250 },
+        { value: 0.5, easing: "easeInOutQuad", duration: 500 },
+      ],
+      delay: anime.stagger(100, {
+        grid: [GRID_WIDTH, GRID_HEIGHT],
+        from: e.target.dataset.index,
+      }),
+    });
+  };
+
+  const dots = [];
+  let index = 0;
+
+  for (let i = 0; i < GRID_WIDTH; i++) {
+    for (let j = 0; j < GRID_HEIGHT; j++) {
+      dots.push(
+        <GridItem
+          className="group"
+          cursor="crosshair"
+          rounded="full"
+          p={2}
+          transition="background-color 0.2s"
+          _hover={{ bg: "green.200" }}
+          data-index={index}
+          key={`${i}-${j}`}
+        >
+          <Box
+            className="dot-point"
+            h={2}
+            w={2}
+            rounded="full"
+            bgGradient="linear(to-b, green.700, green.400)"
+            opacity={0.5}
+            _groupHover={{
+              bgGradient: "linear(to-b, indigo.600, white)",
+            }}
+            data-index={index}
+          />
+        </GridItem>
+      );
+      index++;
+    }
+  }
+
+  return (
+    <Grid
+      onClick={handleDotClick}
+      templateColumns={`repeat(${GRID_WIDTH}, 1fr)`}
+      w="fit-content"
+    >
+      {dots}
+    </Grid>
+  );
+};
+
+export default WaterDropGrid;
